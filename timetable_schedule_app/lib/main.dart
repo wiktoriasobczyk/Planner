637671444bdb9b06b2f5fb42dc19ada5fd028121
wsobczyk <wiktoriasobczyk@gmail.com>
2019-12-03 00:53:45 +0100

    ovrride login and register

--- timetable_schedule_app/lib/main.dart
+++ timetable_schedule_app/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       routes:{
-        '/': (context) => LoginPage(title: 'Login home page'),
+        '/': (context) => LoginPage(),
         '/second': (context) => SecondScreen(),
       } 
     );
--- timetable_schedule_app/lib/src/home/view/home.dart
+++ timetable_schedule_app/lib/src/home/view/home.dart
@@ -1,191 +1,75 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:timetable_schedule_app/src/scheduler/view.dart';
 
-class LoginPage extends StatefulWidget {
-  LoginPage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _LoginPageState createState() => _LoginPageState();
-}
+const users = const {
+  'dribbble@gmail.com': '12345',
+  'hunter@gmail.com': 'hunter',
+};
 
-// Used for controlling whether the user is loggin or creating an account
-enum FormType { login, register }
-
-class _LoginPageState extends State<LoginPage> {
-  final TextEditingController _emailFilter = new TextEditingController();
-  final TextEditingController _passwordFilter = new TextEditingController();
-  final TextEditingController _usernameFilter = new TextEditingController();
-  String _username = "";
-  String _email = "";
-  String _password = "";
-
-  FormType _form = FormType
-      .login; // our default setting is to login, and we should switch to creating an account when the user chooses to
-
-  _LoginPageState() {
-    _emailFilter.addListener(_emailListen);
-    _passwordFilter.addListener(_passwordListen);
-    _usernameFilter.addListener(_usernameListen);
-  }
-
-  void _emailListen() {
-    if (_emailFilter.text.isEmpty) {
-      _email = "";
-    } else {
-      _email = _emailFilter.text;
-      print(_email);
-    }
-  }
-
-  void _usernameListen() {
-    if (_usernameFilter.text.isEmpty) {
-      _username = "";
-      print('pusto w chuj');
-    } else {
-      _username = _usernameFilter.text;
-      print(_username);
-    }
-  }
-
-  void _passwordListen() {
-    if (_passwordFilter.text.isEmpty) {
-      _password = "";
-    } else {
-      _password = _passwordFilter.text;
-    }
-  }
-
-  // Swap in between our two forms, registering and logging in
-  void _formChange() async {
-    setState(() {
-      if (_form == FormType.register) {
-        _form = FormType.login;
-      } else {
-        _form = FormType.register;
-        print('register form');
+class LoginPage extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: 2250);
+  Future<String> _authUser(LoginData data) {
+    print('Name: ${data.name}, Password: ${data.password}');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(data.name)) {
+        return 'Podana nazwa użytkownika nie istnieje';
       }
+      if (users[data.name] != data.password) {
+        return 'Hasła do siebie nie pasują';
+      }
+      return null;
+    });
+  }
+
+  Future<String> _recoverPassword(String name) {
+    print('Name: $name');
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return 'Podana nazwa użytkownika nie istnieje';
+      }
+      return null;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return FlutterLogin(
+      title: 'Planner',
+      onLogin: _authUser,
+      onSignup: _authUser,
+      onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(
+          builder: (context) => SecondScreen(),
+        ));
+      },
+      emailValidator: (value) {
+        if (!value.contains('@') || !value.endsWith('.com')) {
+          return "Email musi zawierać '@''";
+        }
+        return null;
+      },
+      passwordValidator: (value) {
+        if (value.isEmpty) {
+          return 'Hasło nie może być puste';
+        }
+        return null;
+      },
+      showDebugButtons: false,
+      onRecoverPassword: _recoverPassword,
+      messages: LoginMessages(
+        usernameHint: 'Nazwa użytkownika',
+        passwordHint: 'Hasło',
+        confirmPasswordHint: 'Wpisz ponownie hasło',
+        loginButton: 'ZALOGUJ SIĘ',
+        signupButton: 'REJESTRACJA',
+        goBackButton: 'Powrót',
+        forgotPasswordButton: 'Zapomniałeś hasła?',
+        confirmPasswordError: 'Hasła do siebie nie pasują',
+        recoverPasswordDescription:
+            'Aby otrzymać ponownie hasło musisz podać mail użyty podczas rejestracji',
+        recoverPasswordSuccess: 'Email z potwierdzeniem został wysłany',
       ),
-      body: new Container(
-        padding: EdgeInsets.all(16.0),
-        child: new Column(
-          children: <Widget>[
-            _buildTextFields(),
-            _buildButtons(),
-          ],
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
-  }
-
-  Widget _buildButtons() {
-    if (_form == FormType.login) {
-      return new Container(
-        child: new Column(
-          children: <Widget>[
-            new RaisedButton(
-              child: new Text('Login'),
-              onPressed: _loginPressed,
-            ),
-            new FlatButton(
-              child: new Text('Dont have an account? Tap here to register.'),
-              onPressed: _formChange,
-            ),
-            new FlatButton(
-              child: new Text('Forgot Password?'),
-              onPressed: _passwordReset,
-            )
-          ],
-        ),
-      );
-    } else {
-      return new Container(
-        child: new Column(
-          children: <Widget>[
-            new RaisedButton(
-              child: new Text('Create an Account'),
-              onPressed: _createAccountPressed,
-            ),
-            new FlatButton(
-              child: new Text('Have an account? Click here to login.'),
-              onPressed: _formChange,
-            )
-          ],
-        ),
-      );
-    }
-  }
-
-  Widget _buildTextFields() {
-    if (_form == FormType.login) {
-      return new Container(
-        child: new Column(
-          children: <Widget>[
-            new Container(
-              child: new TextField(
-                controller: _emailFilter,
-                decoration: new InputDecoration(labelText: 'Email'),
-              ),
-            ),
-            new Container(
-              child: new TextField(
-                controller: _passwordFilter,
-                decoration: new InputDecoration(labelText: 'Password'),
-                obscureText: true,
-              ),
-            )
-          ],
-        ),
-      );
-    } else {
-           print('render register');
-      return new Container(
-        child: new Column(
-          children: <Widget>[
-            new Container(
-              child: new TextField(
-                controller: _usernameFilter,
-                decoration: new InputDecoration(labelText: 'Username^2'),
-              ),
-            ),
-            new Container(
-              child: new TextField(
-                controller: _emailFilter,
-                decoration: new InputDecoration(labelText: 'Email'),
-              ),
-            ),
-            new Container(
-              child: new TextField(
-                controller: _passwordFilter,
-                decoration: new InputDecoration(labelText: 'Password'),
-                obscureText: true,
-              ),
-            )
-          ],
-        ),
-      );
-    }
-  }
-  // These functions can self contain any user auth logic required, they all have access to _email and _password
-
-  void _loginPressed() {
-    Navigator.pushNamed(context, '/second');
-    print('The user wants to login with $_email and $_password');
-  }
-
-  void _createAccountPressed() {
-    print('The user wants to create an accoutn with $_email and $_password');
-  }
-
-  void _passwordReset() {
-    print("The user wants a password reset request sent to $_email");
   }
 }
