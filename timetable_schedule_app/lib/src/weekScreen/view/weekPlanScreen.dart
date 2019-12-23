d903368bfd4c70ddcce48b5d835229bbbb48e4a7
wsobczyk <wiktoriasobczyk@gmail.com>
2019-12-23 18:45:20 +0100

    add mockup drawer

--- timetable_schedule_app/lib/src/weekScreen/view/weekPlanScreen.dart
+++ timetable_schedule_app/lib/src/weekScreen/view/weekPlanScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'package:timetable_schedule_app/src/graphqlTest/controller.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class WeekPlanScreen extends StatefulWidget {
   @override
@@ -32,6 +33,56 @@ class _WeekPlanScreenState extends State<WeekPlanScreen> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Plan tygodnia'),
+      ),
+      drawer: Drawer(child: Column(
+          children: <Widget>[
+            UserAccountsDrawerHeader(
+              accountEmail: Text("w@w.pl"),
+              accountName: Text("Wiktoria Sobczyk"),
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: NetworkImage(
+                    "https://icons-for-free.com/iconfiles/png/512/female+person+user+woman+young+icon-1320196266256009072.png"),
+              ),
+              otherAccountsPictures: <Widget>[
+                GestureDetector(
+                  onTap: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AlertDialog(
+                              title: Text("Adding new account..."));
+                        });
+                  },
+                  child: CircleAvatar(
+                    child: Icon(Icons.add),
+                  ),
+                )
+              ],
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.clipboardList),
+              title: Text("Twój plan"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.search),
+              title: Text("Wyszukaj organizacje"),
+            ),
+            ListTile(
+              leading: Icon(FontAwesomeIcons.calendarPlus),
+              title: Text("Dodaj zajęcia"),
+            ),
+            Divider(),
+            Expanded(
+              child: Align(
+                alignment: FractionalOffset.bottomCenter,
+                child: ListTile(
+                  leading: Icon(FontAwesomeIcons.signOutAlt),
+                  title: Text("Wyloguj się"),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
       body: Center(
         child: ListView.builder(
