92f50b61380f5d332116ee9a311bcb2b5f8d11a1
wsobczyk <wiktoriasobczyk@gmail.com>
2019-12-28 09:06:07 +0100

    change test button, fix bug

--- timetable_schedule_app/lib/src/weekScreen/view/weekPlanScreen.dart
+++ timetable_schedule_app/lib/src/weekScreen/view/weekPlanScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:timetable_schedule_app/src/common/green_button.dart';
 // import 'package:timetable_schedule_app/src/graphqlTest/controller.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:timetable_schedule_app/src/drawer/view.dart';
@@ -146,26 +147,14 @@ class _WeekPlanScreenState extends State<WeekPlanScreen> {
                                 ],
                               ),
                               //Button
-                          Padding(
-                            padding: const EdgeInsets.only(top: 3.0),
-                            child: RaisedButton(
-                                color: new Color(0xffffffff),
-                                child: Text(
-                                  'Mockup danych',
-                                  style: TextStyle(
-                                    color: new Color(0xff6200ee),
-                                  ),
-                                ),
-                                onPressed: () {
-                                  Navigator.of(context)
-                                      .pushReplacement(MaterialPageRoute(
-                                    builder: (context) => WeekPlanScreen(),
-                                  ));
-                                },
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(30.0),
-                                )),
-                          ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 3.0),
+                              child: GreenButton(
+                              labelText: 'Mockup',
+                              onPressed: () => {
+                                print('before graphql')
+                              },),
+                                     )
                             ])
                       ],
                     ),
