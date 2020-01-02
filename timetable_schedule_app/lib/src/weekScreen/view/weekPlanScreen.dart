2559db8e55b4b84a3cde9039f45cfd21071915b4
wsobczyk <wiktoriasobczyk@gmail.com>
2020-01-02 16:10:44 +0100

    added countries as a listview in plan week

--- timetable_schedule_app/lib/src/weekScreen/view/weekPlanScreen.dart
+++ timetable_schedule_app/lib/src/weekScreen/view/weekPlanScreen.dart
@@ -49,177 +49,179 @@ class _WeekPlanScreenState extends State<WeekPlanScreen> {
         title: Text('Plan tygodnia'),
       ),
       drawer: DrawerApp(),
-      body: Center(
-        child: ListView.builder(
-          itemCount: days.length,
-          scrollDirection: Axis.horizontal,
-          shrinkWrap: true,
-          itemBuilder: (context, index) {
-            return Padding(
-                padding: const EdgeInsets.all(5.0),
-                child: Wrap(alignment: WrapAlignment.center, children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(5.0),
-                      color: dayColor[index],
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only(top: 8.0),
-                                child: Container(
-                                  child: Text(
-                                    days[index], // day
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 24.0,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Container(
-                                child: Text(
-                                  'Czas trawnia twoich zajęc',
-                                  style: TextStyle(
-                                    color: Colors.white54,
-                                    fontSize: 20.0,
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: const EdgeInsets.only(top: 8.0),
-                                child: Container(
-                                    width: 150.0,
-                                    height: 130.0,
-                                    decoration: new BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      color: Colors.white,
-                                    ),
-                                    child: Column(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.center,
-                                      children: <Widget>[
-                                        Container(
-                                          child: Text(
-                                            hours[index] + ' h',
-                                            style: TextStyle(
-                                              fontSize: 30.0,
-                                              color: Color.fromRGBO(
-                                                  128, 142, 149, 1),
-                                              fontWeight: FontWeight.bold,
-                                            ),
-                                          ),
-                                        ),
-                                      ],
-                                    )),
-                              ),
-                              FutureBuilder(
-                                  future: countries,
-                                  builder: (BuildContext context,
-                                      AsyncSnapshot snapshot) {
-                                    switch (snapshot.connectionState) {
-                                      case ConnectionState.none:
-                                      case ConnectionState.waiting:
-                                      case ConnectionState.active:
-                                        return Center(
-                                            child: CircularProgressIndicator());
-                                      case ConnectionState.done:
-                                        if (snapshot.hasError)
-                                          return Text(
-                                              "There was an error: ${snapshot.error}");
-                                        return ListView.builder(
-                                          itemCount: 2,
-                                          itemBuilder: (BuildContext context,
-                                              int index) {
-                                            DataTable(columns: [
-                                              DataColumn(
-                                                  label: Text('Godzina')),
-                                              DataColumn(
-                                                  label: Text('Przedmiot')),
-                                              DataColumn(
-                                                  label: Text('Miejsce')),
-                                            ], rows: [
-                                              DataRow(cells: [
-                                                DataCell(Text('8:00 - 10:00')),
-                                                DataCell(Text('Seminarium')),
-                                                DataCell(Text('PŁ')),
-                                              ])
-                                            ]);
-                                          },
-                                        );
-                                    }
-                                  }),
-                              // DataTable(
-                              //   columns: [
-                              //     DataColumn(label: Text('Godzina')),
-                              //     DataColumn(label: Text('Przedmiot')),
-                              //     DataColumn(label: Text('Miejsce')),
-                              //   ],
-                              //   rows:
-                              //    [
-                              //     DataRow(cells: [
-                              //       DataCell(Text('8:00 - 10:00')),
-                              //       DataCell(Text('Seminarium')),
-                              //       DataCell(Text('PŁ')),
-                              //     ]),
-                              //     DataRow(cells: [
-                              //       DataCell(Text('11:00 - 12:00')),
-                              //       DataCell(Text('Fizyka')),
-                              //       DataCell(Text('CTI 102')),
-                              //     ]),
-                              //     DataRow(cells: [
-                              //       DataCell(Text('11:00 - 12:00')),
-                              //       DataCell(Text('Fizyka')),
-                              //       DataCell(Text('CTI 102')),
-                              //     ]),
-                              //     DataRow(cells: [
-                              //       DataCell(Text('11:00 - 12:00')),
-                              //       DataCell(Text('Fizyka')),
-                              //       DataCell(Text('CTI 102')),
-                              //     ]),
-                              //     DataRow(cells: [
-                              //       DataCell(Text('11:00 - 12:00')),
-                              //       DataCell(Text('Fizyka')),
-                              //       DataCell(Text('CTI 102')),
-                              //     ]),
-                              //     DataRow(cells: [
-                              //       DataCell(Text('11:00 - 12:00')),
-                              //       DataCell(Text('Fizyka')),
-                              //       DataCell(Text('CTI 102')),
-                              //     ]),
-                              //   ],
-                              // ),
-                              // //Button
-                              Padding(
-                                padding: const EdgeInsets.only(top: 3.0),
-                                child: GreenButton(
-                                  labelText: 'Mockup',
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                    Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                        builder: (BuildContext context) =>
-                                            GraphqlTest(),
-                                      ),
-                                    );
-                                  },
-                                ),
-                              )
-                            ])
-                      ],
-                    ),
-                  )
-                ]));
+     body: 
+          FutureBuilder(
+          future: countries,
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+              case ConnectionState.active:
+                return Center(child: CircularProgressIndicator());
+              case ConnectionState.done:
+                if (snapshot.hasError)
+                  return Text("There was an error: ${snapshot.error}");
+                var countries = snapshot.data;
+                return ListView.separated(
+                  itemCount: countries.length,
+                  separatorBuilder: (context, index) => Divider(),
+                  itemBuilder: (BuildContext context, int index) {
+                    Country message = countries[index];
+
+                    return ListTile(
+                      title: Text(message.name),
+                      isThreeLine: true,
+                      leading: CircleAvatar(
+                        child: Text('PJ'), 
+                      ),
+                      subtitle: Text(
+                        message.nativeLang,
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    );
+                  },
+                );
+            }
           },
-        ),
-      ),
-    );
+        ));
+ 
+    // body: Center(
+    //   child: ListView.builder(
+    //     itemCount: days.length,
+    //     scrollDirection: Axis.horizontal,
+    //     shrinkWrap: true,
+    //     itemBuilder: (context, index) {
+    //       return Padding(
+    //           padding: const EdgeInsets.all(5.0),
+    //           child: Wrap(alignment: WrapAlignment.center, children: [
+    //             Container(
+    //               decoration: BoxDecoration(
+    //                 borderRadius: BorderRadius.circular(5.0),
+    //                 color: dayColor[index],
+    //               ),
+    //               child: Row(
+    //                 mainAxisAlignment: MainAxisAlignment.center,
+    //                 children: <Widget>[
+    //                   Column(
+    //                       mainAxisAlignment: MainAxisAlignment.center,
+    //                       children: <Widget>[
+    //                         Padding(
+    //                           padding: const EdgeInsets.only(top: 8.0),
+    //                           child: Container(
+    //                             child: Text(
+    //                               days[index], // day
+    //                               style: TextStyle(
+    //                                 color: Colors.white,
+    //                                 fontSize: 24.0,
+    //                                 fontWeight: FontWeight.bold,
+    //                               ),
+    //                             ),
+    //                           ),
+    //                         ),
+    //                         Container(
+    //                           child: Text(
+    //                             'Czas trawnia twoich zajęc',
+    //                             style: TextStyle(
+    //                               color: Colors.white54,
+    //                               fontSize: 20.0,
+    //                             ),
+    //                           ),
+    //                         ),
+    //                         Padding(
+    //                           padding: const EdgeInsets.only(top: 8.0),
+    //                           child: Container(
+    //                               width: 150.0,
+    //                               height: 130.0,
+    //                               decoration: new BoxDecoration(
+    //                                 shape: BoxShape.circle,
+    //                                 color: Colors.white,
+    //                               ),
+    //                               child: Column(
+    //                                 mainAxisAlignment:
+    //                                     MainAxisAlignment.center,
+    //                                 children: <Widget>[
+    //                                   Container(
+    //                                     child: Text(
+    //                                       hours[index] + ' h',
+    //                                       style: TextStyle(
+    //                                         fontSize: 30.0,
+    //                                         color: Color.fromRGBO(
+    //                                             128, 142, 149, 1),
+    //                                         fontWeight: FontWeight.bold,
+    //                                       ),
+    //                                     ),
+    //                                   ),
+    //                                 ],
+    //                               )),
+    //                         ),
+
+    //                         // DataTable(
+    //                         //   columns: [
+    //                         //     DataColumn(label: Text('Godzina')),
+    //                         //     DataColumn(label: Text('Przedmiot')),
+    //                         //     DataColumn(label: Text('Miejsce')),
+    //                         //   ],
+    //                         //   rows:
+    //                         //    [
+    //                         //     DataRow(cells: [
+    //                         //       DataCell(Text('8:00 - 10:00')),
+    //                         //       DataCell(Text('Seminarium')),
+    //                         //       DataCell(Text('PŁ')),
+    //                         //     ]),
+    //                         //     DataRow(cells: [
+    //                         //       DataCell(Text('11:00 - 12:00')),
+    //                         //       DataCell(Text('Fizyka')),
+    //                         //       DataCell(Text('CTI 102')),
+    //                         //     ]),
+    //                         //     DataRow(cells: [
+    //                         //       DataCell(Text('11:00 - 12:00')),
+    //                         //       DataCell(Text('Fizyka')),
+    //                         //       DataCell(Text('CTI 102')),
+    //                         //     ]),
+    //                         //     DataRow(cells: [
+    //                         //       DataCell(Text('11:00 - 12:00')),
+    //                         //       DataCell(Text('Fizyka')),
+    //                         //       DataCell(Text('CTI 102')),
+    //                         //     ]),
+    //                         //     DataRow(cells: [
+    //                         //       DataCell(Text('11:00 - 12:00')),
+    //                         //       DataCell(Text('Fizyka')),
+    //                         //       DataCell(Text('CTI 102')),
+    //                         //     ]),
+    //                         //     DataRow(cells: [
+    //                         //       DataCell(Text('11:00 - 12:00')),
+    //                         //       DataCell(Text('Fizyka')),
+    //                         //       DataCell(Text('CTI 102')),
+    //                         //     ]),
+    //                         //   ],
+    //                         // ),
+    //                         // //Button
+    //                         Padding(
+    //                           padding: const EdgeInsets.only(top: 3.0),
+    //                           child: GreenButton(
+    //                             labelText: 'Mockup',
+    //                             onPressed: () {
+    //                               Navigator.pop(context);
+    //                               Navigator.push(
+    //                                 context,
+    //                                 MaterialPageRoute(
+    //                                   builder: (BuildContext context) =>
+    //                                       GraphqlTest(),
+    //                                 ),
+    //                               );
+    //                             },
+    //                           ),
+    //                         )
+    //                       ])
+    //                 ],
+    //               ),
+    //             )
+    //           ]));
+    //     },
+    //   ),
+    // ),
+    // );
   }
 }
 
